5b697e3eec4abefba39263a876f10a737c2896f4
duarteocarmo <me@duarteocarmo.com>
2025-12-31 19:42:43 +0000

    update

--- limpa/services/s3.py
+++ limpa/services/s3.py
@@ -6,9 +6,10 @@ import boto3
 def get_s3_client():
     return boto3.client(
         "s3",
+        endpoint_url=os.environ.get("AWS_ENDPOINT_URL"),
         aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        region_name=os.environ.get("AWS_S3_REGION", "us-east-1"),
+        region_name=os.environ.get("AWS_S3_REGION", "auto"),
     )
 
 
